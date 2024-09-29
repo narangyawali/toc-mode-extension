14ab03da8c40fb71ac8095b57adba6f40eb3f655
naran gyawali <nryng123@gmail.com>
2024-09-29 14:31:46 +0545

    added another functionality

--- background.js
+++ background.js
@@ -1,16 +1,3 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
@@ -64,3 +51,46 @@ chrome.action.onClicked.addListener(async (tab) => {
       // });
     }
 });
+
+
+// for search the meaning 
+//
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "searchMeaning",
+    title: "Search meaning of '%s'",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "searchMeaning") {
+    const query = info.selectionText;
+    const searchUrl = `https://www.google.com/search?q=meaning+of+${encodeURIComponent(query)}`;
+    chrome.tabs.create({ url: searchUrl });
+  }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "search-meaning") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: searchHighlightedText
+      });
+    });
+  }
+});
+
+function searchHighlightedText() {
+  const selection = window.getSelection().toString().trim();
+  if (selection) {
+    const searchUrl = `https://www.google.com/search?q=meaning+of+${encodeURIComponent(selection)}`;
+    window.open(searchUrl, '_blank');
+  } else {
+    alert("Please highlight a word or phrase to search for its meaning.");
+  }
+}
+
+
